677f0f390c796fb6eedbeba736537b11aa4cc315
Sreejith Njamelil <sree127@gmail.com>
2021-06-14 23:32:53 +0200

    pod deintegrate

--- Pods/Headers/Public/RxFlow/RxFlow-umbrella.h
+++
@@ -1,1 +0,0 @@
-../../../Target Support Files/RxFlow/RxFlow-umbrella.h
--- Pods/Target Support Files/Pods-GithubFavs/Pods-GithubFavs-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GithubFavs : NSObject
-@end
-@implementation PodsDummy_Pods_GithubFavs
-@end
